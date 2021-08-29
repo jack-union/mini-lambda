da4ea7f3f748ee8629215a24702140a0677a8db5
jack union <code@hassfeld.net>
2021-08-29 16:27:50 +0200

    Display cold sensor info and little code autoformat

--- 10_display.ino
+++ 10_display.ino
@@ -6,7 +6,7 @@
 #define LARGE_FONT u8g2_font_logisoso46_tn
 
 void initDisplay() {
-  display.setI2CAddress(0x78); //0x3c * 2, default
+  display.setI2CAddress(0x78);  //0x3c * 2, default
   display.begin();
   display.setDrawColor(1);
   display.setFontMode(0);
@@ -14,7 +14,7 @@ void initDisplay() {
   display.firstPage();
   do {
     draw_logo();
-  } while ( display.nextPage() );
+  } while (display.nextPage());
 }
 
 void draw_lambda(uint16_t readout) {
@@ -23,7 +23,13 @@ void draw_lambda(uint16_t readout) {
 
   // connection valid?
   if (lambdaError == 255) {
-    sprintf(data, "%03d", readout);
+    if (lambdaStatus & 0b00100000) {  // temp reached
+      sprintf(data, "%03d", readout);
+    } else {
+      data[0] = "-";
+      data[1] = "-";
+      data[2] = "-";
+    }
 
     // display sensor reading
     display.setFont(LARGE_FONT);
@@ -34,19 +40,19 @@ void draw_lambda(uint16_t readout) {
     display.print(data[2]);
 
     // status infos
-    if (lambdaStatus & 0b00100000) { // temp reached
+    if (lambdaStatus & 0b00100000) {  // temp reached
       info = " ";
     }
-    if (lambdaStatus & 0b10000000) { // heating
+    if (lambdaStatus & 0b10000000) {  // heating
       info = "H";
     }
-    if (lambdaStatus & 0b00001000) { // voltage too high
+    if (lambdaStatus & 0b00001000) {  // voltage too high
       info = "U ++";
     }
-    if (lambdaStatus & 0b00000100) { // voltage too low
+    if (lambdaStatus & 0b00000100) {  // voltage too low
       info = "U --";
     }
-    if (lambdaStatus & 0b00000010) { // temp too high
+    if (lambdaStatus & 0b00000010) {  // temp too high
       info = "T ++";
     }
 
@@ -57,25 +63,29 @@ void draw_lambda(uint16_t readout) {
 
     // show hint
     display.setCursor(80, 61);
-    if (readout < 100 ) {
-      display.println(F("rich"));
+    if (~lambdaStatus & 0b00100000) {  // temp not reached
+      display.println(F("cold"));
     } else {
-      display.println(F("lean"));
+      if (readout < 100) {
+        display.println(F("rich"));
+      } else {
+        display.println(F("lean"));
+      }
     }
-  } else { // no valid connection
+  } else {  // no valid connection
     display.setFont(SMALL_FONT);
     display.setCursor(20, 46);
-    if (lambdaError == 127) { // no sensor
+    if (lambdaError == 127) {  // no sensor
       display.println(F("no sensor"));
-    } else { // all other errors
+    } else {  // all other errors
       display.println(F("no data"));
     }
-  }  
+  }
 }
 
 void draw_logo() {
   display.drawXBMP(
-    (SCREEN_WIDTH - LOGO_WIDTH ) / 2,
+    (SCREEN_WIDTH - LOGO_WIDTH) / 2,
     (SCREEN_HEIGHT - LOGO_HEIGHT) / 2,
     LOGO_WIDTH, LOGO_HEIGHT,
     logo_bmp);
